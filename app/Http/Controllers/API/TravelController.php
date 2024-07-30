07e404879fc4a31c06674bb1eb4044c07c489e2b
StefanoDaniello <157887560+StefanoDaniello@users.noreply.github.com>
2024-07-30 12:26:53 +0200

    add creazione

--- app/Http/Controllers/API/TravelController.php
+++ app/Http/Controllers/API/TravelController.php
@@ -15,7 +15,7 @@ class TravelController extends Controller
             'name' => 'required|string|max:255',
             'description' => 'nullable|string',
             'start_date' => 'required|date',
-            'end_date' => 'required|date|after_or_equal:start_date',
+            'end_date' => 'required|date',
             'image' => 'nullable|string',
             'meal' => 'nullable|string',
             'curiosity' => 'nullable|string',
