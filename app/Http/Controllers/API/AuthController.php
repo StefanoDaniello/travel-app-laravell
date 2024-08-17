d4d7ba3590b28b23a4090b57732d5f09403abc8a
StefanoDaniello <157887560+StefanoDaniello@users.noreply.github.com>
2024-08-17 19:45:42 +0200

    add message

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -15,14 +15,44 @@ class AuthController extends Controller
     public function register(Request $request)
     {
         $validator = Validator::make($request->all(), [
-            'name' => 'required|string|max:50',
-            'email' => 'required|string|email|max:100|unique:users',
-            'password' => 'required|string|min:8|max:50',
+            'name' => 'required|max:50',
+            'email' => 'required|email|max:100|unique:users',
+            'password' => 'required|min:8|max:50',
+        ], [
+            'name.required' => 'Il campo nome è obbligatorio.',
+            'name.max' => 'Il nome non può superare i 50 caratteri.',
+            
+            'email.required' => 'Il campo email è obbligatorio.',
+            'email.email' => 'L\'email deve essere un indirizzo email valido.',
+            'email.max' => 'L\'email non può superare i 100 caratteri.',
+            'email.unique' => 'L\'email inserita è già registrata.',
+            
+            'password.required' => 'Il campo password è obbligatorio.',
+            'password.min' => 'La password deve contenere almeno 8 caratteri.',
+            'password.max' => 'La password non può superare i 50 caratteri.',
         ]);
+        
+        
+    if ($validator->fails()) {
+        // Personalizzazione dei messaggi di errore
+        $errors = $validator->errors()->all();
+        $formattedErrors = [];
 
-        if ($validator->fails()) {
-            return response()->json(['errors' => $validator->errors()], 422);
+        foreach ($errors as $error) {
+            if (strpos($error, 'name') !== false) {
+                $formattedErrors[] =  $error;
+            } elseif (strpos($error, 'email') !== false) {
+                $formattedErrors[] = $error;
+            } elseif (strpos($error, 'password') !== false) {
+                $formattedErrors[] =  $error;
+            } else {
+                $formattedErrors[] = $error;
+            }
         }
+
+        return response()->json(['errors' => $formattedErrors], 422);
+    }
+        
 
         $user = User::create([
             'name' => $request->name,
@@ -40,8 +70,8 @@ class AuthController extends Controller
     public function login(Request $request)
     {
         $validator = Validator::make($request->all(), [
-            'email' => 'required|string|email|max:100',
-            'password' => 'required|string|min:8|max:50',
+            'email' => 'required|email|max:100',
+            'password' => 'required|min:8|max:50',
         ]);
 
         if ($validator->fails()) {
